485af069d38eb0ee91a6e1a190ada95a85a1cf3d
tcheng9 <tcheng789@gmail.com>
2022-11-10 21:44:51 -0600

    facebook login is working on front end and sending over user data through res.json but not sure how to use it for the front end

--- frontend-odinbook/src/components/RouteSwitch.js
+++ frontend-odinbook/src/components/RouteSwitch.js
@@ -6,13 +6,14 @@ import App from "../App";
 import Home from "./home"
 import Signup from "./signup"
 import Login from "./login";
+import FacebookLogin from "./facebookLogin";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={[<App />, <Login />]} />
-        
+        <Route path = "/facebook" element = {<FacebookLogin />} />
         <Route path = "/home" element = {<Home />} />
         <Route path = "/signup" element = {<Signup />} />
       </Routes>
