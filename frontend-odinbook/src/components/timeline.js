09096384c69ca4fcddc89320efd7a5ab3211ac9e
Tommy C <tcheng789@gmail.com>
2022-11-12 23:51:55 -0600

    got GET posts working w/ css, now to work on comments and likes system

--- frontend-odinbook/src/components/timeline.js
+++ frontend-odinbook/src/components/timeline.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import {useEffect, useState} from 'react';
+import "./timeline.css";
 
 const Timeline = () => {
 
-    const [postdata, setPostData] = useState('');
+    const [postData, setPostData] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/posts')
-        .then((response) => {response.json()})
-        .then((data) => {
-            setPostData(data);
-            console.log(postdata);
-        })
-
-        }, [])
+        postList()
+    }, [])
     
+    const postList = async () => {
+        const response = await fetch("http://localhost:3000/posts");
 
+        setPostData(await response.json())
+        
+    }
   
     
     return (
         <div>
             <h1> Hello from timeline </h1>    
+            <ol>
 
+           
+            {postData.map((data) => {
+                return (
+                    <li className = "postItem" key = {data.id}> {data.title} </li>
+                )
+            })}
+            </ol>
             <a href = "/createpost"> Create a post </a>        
         </div>
     )
