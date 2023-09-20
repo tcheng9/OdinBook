606ea6efeff75315f07b65fda85056891770b8b2
tommy <tcheng789@gmail.com>
2023-09-20 13:16:26 -0500

    finished backend unlike functionality. Working on frontend like and post/comment delete code

--- backend-odinbook/controllers/postController.js
+++ backend-odinbook/controllers/postController.js
@@ -118,7 +118,8 @@ exports.post_likes = (req, res, next) => {
         if (err){
             throw err;
         } else {
-            res.status(201).json({response: req.params.userId});
+            // res.status(201).json({response: req.params.userId});
+            res.status(201).json({response: `appended this id: ${userId}`});
         }
             
         
@@ -127,6 +128,77 @@ exports.post_likes = (req, res, next) => {
     
 }
 
+// POST unlike functionality
+
+exports.post_unlikes = async (req, res, next) => {
+    let postId = req.params.postId;
+    let userId = req.body.userId;
+    
+    // res.status(200).json({name: userId});
+    try {
+        await res.post;
+        //likes_arr stores the likes for the current post
+        let likes_arr = res.post.likes;
+       
+        // return res.status(200).json({likes: likes_arr});
+        if (likes_arr.length != 0){
+            
+            for (let i = 0; i < likes_arr.length; i++){
+                // likes_arr[i] = '650128b1fa256bb871fb51ac';
+                // userId = '650128b1fa256bb871fb51ac';
+                // res.json({
+                //     likes: likes_arr,
+                //     userId: userId
+                // })
+                // likesArr = ['test1', 'test2', '650128b1fa256bb871fb51ac']
+
+                if (likes_arr[i] === userId ){
+                    likes_arr.splice(i,1);
+                   
+                   
+                    Post.findByIdAndUpdate(postId, {$set: {likes: likes_arr}}, function(err, data) {
+                        if (err){
+                            throw err;
+                        } else {
+                            res.status(201).json({message: likes_arr});
+                        }
+                            
+                        
+                    })
+                    
+                }
+            }
+
+            res.json({message: 'no matches found'})
+        }
+
+    } catch (err){
+        res.json({message: err.message});
+    }
+    
+
+   
+    
+}
+
+exports.get_likes_by_id = async (req, res, next) => {
+    //this is the same as Get Likes (because they both search by id) 
+    //BUT this is for a middleware function
+    
+    let postId = req.params.postId;
+    let post;
+    try {
+        post = await Post.findById(req.params.postId);
+        if (post === null){
+            return res.json({message: `cannot find post ${post}`});
+        }
+    } catch (err){
+        return res.status(401).json({message: err.message});
+    }
+    
+    res.post = post;
+    next();
+}
 exports.get_post_by_id = async function getPost(req, res, next){
     let post;
     try{
--- backend-odinbook/routes/createPost.js
+++ backend-odinbook/routes/createPost.js
@@ -6,7 +6,7 @@ var post_controller = require('../controllers/postController');
 /////////////
 /*
 
-THIS PAGE CONTAINS ALL POST FUNCTIONALITY
+THIS PAGE CONTAINS ALL POST & LIKES FUNCTIONALITY
 */
 
 ////////////////
@@ -22,11 +22,7 @@ router.post('/create', post_controller.make_post)
 
 router.get('/:id', post_controller.get_details);
 
-/* GET likes for a  specfici post; */
-router.get('/:postId/likes', post_controller.get_likes);
 
-/* POST likes for a specific post */ 
-router.post('/:postId/likes', post_controller.post_likes);
 
 /* UPDATE A POST */
 router.patch('/:id', post_controller.get_post_by_id, post_controller.update_post);
@@ -34,4 +30,26 @@ router.patch('/:id', post_controller.get_post_by_id, post_controller.update_post
 /* DELETE A POST */
 router.delete('/:id', post_controller.get_post_by_id, post_controller.delete_post);
 
+
+///////////////////LIKES FUNCTIONALITY -> SO MAD I NESTED IT HERE
+
+
+/* GET likes for a  specfici post; */
+router.get('/:postId/likes', post_controller.get_likes);
+
+/* POST likes for a specific post */ 
+router.post('/:postId/likes', post_controller.post_likes);
+
+/* GET OR POST? likes for a specific post */
+router.get('/:postId/likes_middleware', post_controller.get_likes_by_id);
+
+/* UNLIKE */
+//GET route was used for initial testing BUT I think I need to use POST for sending data
+router.get('/:postId/unlike', post_controller.get_likes_by_id, post_controller.post_unlikes);
+
+router.post('/:postId/unlike', post_controller.get_likes_by_id, post_controller.post_unlikes);
+
+
+
 module.exports = router;
+
