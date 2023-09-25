38a0357568290d79b6533dfe9e9224d50c92d523
tcheng9 <tcheng789@gmail.com>
2023-09-25 11:29:16 -0500

    friend management buttons and function made. Now need to clean up to work exactly how I need it

--- frontend-odinbook/src/components/friendsManagement.js
+++ frontend-odinbook/src/components/friendsManagement.js
@@ -7,6 +7,7 @@ const FriendsManagement = () => {
     
     const [usersList, setUsersList] = useState([]);
 
+    
     function getAllUsers(){
         //Get all users listed in User's model in the datbase
         const requestOptions = {
@@ -27,12 +28,96 @@ const FriendsManagement = () => {
         })
         
     }
+ ///////////////////////////////////////////////////////////////////////////////////////////////////
+    //Get current user's id to send to mongo db
+    const token = localStorage.getItem('token');
+    const decoded = jwt_decode(token);
+    const userId = decoded.id;
 
+    //On page load, run these functions within useEffect
     useEffect(()=> {
         getAllUsers();
     }, [])
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    //API call to send a pending friend request
+    const sendPendingRequest = (userId, targetId) => {
+        const url = "http://localhost:4000/friends/pending/" + userId + "/" + targetId
+        const fetchSettings = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        fetch(url, fetchSettings);
+    }
 
-  
+    //button handler for sending pending friend request
+    const handlePendingRequest = (e) => {
+        e.preventDefault();
+        
+        sendPendingRequest(userId, e.target.id);
+        console.log(userId + '|||||' + e.target.id);
+        console.log('sent pending friend request')
+    }
+
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    //API call to accept a friend request
+    const acceptFriendRequest = (userId, targetId) => {
+        const url = "http://localhost:4000/friends/accepted/" + userId + "/" + targetId
+        const fetchSettings = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        fetch(url, fetchSettings);
+    }
+    //button handler for sending pending friend request
+    const handleAcceptingFriendRequest = (e) => {
+        e.preventDefault();
+        
+        acceptFriendRequest(userId, e.target.id);
+        console.log(userId + '|||||' + e.target.id);
+        console.log('sent pending friend request')
+    }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    //API call to unfriend a person
+    const unfriendingFunction = (userId, targetId) => {
+        const url = "http://localhost:4000/friends/delete/" + userId + "/" + targetId
+        const fetchSettings = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        fetch(url, fetchSettings);
+    }
+
+    
+
+    //button handler for sending pending friend request
+    const handleUnfriending = (e) => {
+        e.preventDefault();
+        
+        unfriendingFunction(userId, e.target.id);
+        console.log(userId + '|||||' + e.target.id);
+        console.log('sent pending friend request')
+    }
+/*
+Thoughts:
+1. current user is stored locally so you can retrieve that easily
+2. For target id/person you want to friend, it's store in the div id SO you have to get the e.target.id each call 
+-> easy to do as well
+3. Question: How to pass into API call?
+3a. ANSWER: 
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
     return(
         <div>
             {
@@ -42,11 +127,12 @@ const FriendsManagement = () => {
                 <div>
                     {data.username}
                     <br/>
-                    {data._id}
+                    {data._id} 
                     <br/>
                     ------------------------------------------
-                    <button> Add Friend </button>
-                    <button> Unfriend </button>
+                    <button onClick = {handlePendingRequest} id = {data._id}> Add Friend </button>
+                    <button id = {data._id}> Unfriend </button>
+                    <button id = {data._id}> Unfriend </button>
                 </div>
                )
             })
