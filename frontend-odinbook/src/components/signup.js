d0ba20f03780f9c65f6b7935448f440cec12cddd
tommy <tcheng789@gmail.com>
2023-09-27 13:31:20 -0500

    got signup form css done

--- frontend-odinbook/src/components/signup.js
+++ frontend-odinbook/src/components/signup.js
@@ -50,22 +50,36 @@ function Signup(){
 
 
     return (
-        <div className = "signup-form" > 
-            <form className = "form-wrapper" id = "signup">
-                <h1 className = "form-title"> Sign Up </h1>    
-                <div className = "input-wrapper">
-                    <label htmlFor = "username"> Username: </label> <br/>
-                    <input onChange={(e) => handle(e)} value = {data.username} type = "text"  id = "username" name = "username"/> <br/>
+        <div className = "signup-wrapper">
 
+            <div className = "signup-info">
+                <div className = "signup-info-wrapper">
+                    <h1 className = "signup-info-header"> OdinBook </h1>
+                    <div className = "signup-info-text" >
+                        Connect with friends and the world around you on OdinBook
+                    </div>
                 </div>
-                
-                <div className = "input-wrapper">
-                    <label htmlFor = "password"> Password: </label> <br/>
-                    <input onChange={(e) => handle(e)} value = {data.password} type = "text" id = "password" name = "password"/> <br/>
-                </div>
-                
-                <button onClick = {signupSubmit}> Sign Up </button> 
-            </form>
+            </div>
+        
+            <div className = "signup-form" > 
+                <form className = "form-wrapper" id = "signup">
+                    {/* <h1 className = "form-title"> Sign Up </h1>     */}
+                    <div className = "input-wrapper">
+                        {/* <label htmlFor = "username"> Username: </label> <br/> */}
+                        <input onChange={(e) => handle(e)} value = {data.username} type = "text"  id = "username" name = "username" placeholder = "Username">
+                            
+                        </input> 
+
+                    </div>
+                    
+                    <div className = "input-wrapper">
+                        {/* <label htmlFor = "password"> Password: </label> <br/> */}
+                        <input onChange={(e) => handle(e)} value = {data.password} type = "text" id = "password" name = "password" placeholder = "Password"/> 
+                    </div>
+                    
+                    <button className = "form-button" onClick = {signupSubmit}> Sign Up </button> 
+                </form>
+            </div>
         </div>
     )
 }
