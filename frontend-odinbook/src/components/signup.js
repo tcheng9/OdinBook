d19f49b3b499a42f9423c1f14912c781d27567f9
tommy <tcheng789@gmail.com>
2023-09-26 22:06:15 -0500

    css initial stages

--- frontend-odinbook/src/components/signup.js
+++ frontend-odinbook/src/components/signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {useState} from 'react';
 import {useNavigate} from "react-router-dom";
+import './css/signup.css';
 
 function Signup(){
     const navigate = useNavigate();
