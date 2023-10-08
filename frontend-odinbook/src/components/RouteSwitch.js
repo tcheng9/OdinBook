9828e530651aa751018348ceae4f5e576fafa510
tommy <tcheng789@gmail.com>
2023-10-08 14:56:11 -0500

    updates

--- frontend-odinbook/src/components/RouteSwitch.js
+++ frontend-odinbook/src/components/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import React from 'react';
 //Route files
 import App from "../App";
@@ -14,6 +14,7 @@ import DisplayProfiles from "./listAllProfiles";
 import DynamicProfiles from "./DynamicProfiles";
 import FriendsIndex from "./friendsIndex";
 import FriendsManagement from "./friendsManagement";
+
 
 const RouteSwitch = () => {
   return (
@@ -31,7 +32,10 @@ const RouteSwitch = () => {
         <Route path = "/profiles/:id" element = {<DynamicProfiles />} />
         <Route path = "/friends" element = {<FriendsIndex />} />
         <Route path = "/friendsmanagement" element = {<FriendsManagement />} />
-
+        <Route
+            path="/OdinBook"
+            element={<Navigate to="/" replace={true} />}
+        />
       </Routes>
     </BrowserRouter>
   );
